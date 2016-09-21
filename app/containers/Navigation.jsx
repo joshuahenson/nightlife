81a67847d24a987dfbc0884b22f4e18a137a916e
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-09-20 20:25:13 -0700

    Updated brand

--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -11,7 +11,7 @@ const Navigation = ({ user, logOut }) => {
       <Navbar fluid>
         <Header>
           <Brand>
-            <Link to="/">React</Link>
+            <Link to="/">Nightlife</Link>
           </Brand>
           <Toggle />
         </Header>

17cfb52d9f44afa3a3b565b5dd00e4368e26fa93
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-09-21 20:25:06 -0700

    Added user stories

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h6>
+      <h5>
         A sample project built by <a href="https://github.com/joshuahenson">Joshua Henson</a> for improving development skills using universal React while implementing the following user stories.
-      </h6>
+      </h5>
       <ul className="list-unstyled">
-        <li>User Story: -</li>
+        <li>User Story: As an unauthenticated user, I can view all bars in my area.</li>
+        <li>User Story: As an authenticated user, I can add myself to a bar to indicate I am going there tonight.</li>
+        <li>User Story: As an authenticated user, I can remove myself from a bar if I no longer want to go there.</li>
+        <li>User Story: As an unauthenticated user, when I login I should not have to search again.</li>
       </ul>
 
     </div>
